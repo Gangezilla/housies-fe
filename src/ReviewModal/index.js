5ebc4d4830c97890a31f8a50c5cf177e1bb7867f
Scott <scottgangemi@gmail.com>
2017-11-10 19:48:41 +1100

    change form component, i think its better now

--- src/ReviewModal/index.js
+++ src/ReviewModal/index.js
@@ -1,68 +1,73 @@
 import React from 'react';
-import { Form, Text, Radio, RadioGroup } from 'react-form';
+import { withFormik } from 'formik';
 
-const errorValidator = (values) => {
-  return {
-    title: !values.title ||
-           !values.title.match(/title/) ? "Input must contain 'Hello World'" : null,
-  };
-};
+const InnerForm = ({
+  values,
+  errors,
+  touched,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting,
+}) => (
+  <form onSubmit={handleSubmit}>
+    <label htmlFor="Title">Title</label>
+    <input
+      type="title"
+      name="title"
+      onChange={handleChange}
+      onBlur={handleBlur}
+      value={values.title}
+    />
+    {touched.email && errors.email && <div>{errors.email}</div>}
+    <div>
+      <label htmlFor="1" className="">1</label>
+      <input type="radio" value="1" id="1" className="" />
+      <label htmlFor="2" className="">2</label>
+      <input type="radio" value="2" id="2" className="" />
+      <label htmlFor="3" className="">3</label>
+      <input type="radio" value="3" id="3" className="" />
+      <label htmlFor="4" className="">4</label>
+      <input type="radio" value="4" id="4" className="" />
+      <label htmlFor="5" className="">5</label>
+      <input type="radio" value="5" id="5" className="" />
+    </div>
+    <label htmlFor="description">Description</label>
+    <input
+      type="description"
+      name="description"
+      onChange={handleChange}
+      onBlur={handleBlur}
+      value={values.description}
+    />
+    <button type="submit" disabled={isSubmitting}>
+    Submit
+    </button>
+  </form>
+);
 
-const warningValidator = (values) => {
-  return {
-    hello: !values.hello ||
-           !values.hello.match(/^Hello World$/) ? "Input should equal 'Hello World'" : null,
-  };
-};
-
-const successValidator = (values) => {
-  return {
-    hello: values.hello &&
-           values.hello.match(/Hello World/) ? "Thanks for entering 'Hello World'!" : null,
-  };
-};
-
-const submitHandler = (values) => {
-
-};
-
-const ReviewModal = (props) => {
-  return [
-    <span key="currentHome">{JSON.stringify(props.currentHome)}</span>,
-    <Form
-      key="reviewForm"
-      validateWarning={warningValidator}
-      validateSuccess={successValidator}
-      validateError={errorValidator}
-      /* onSubmit={() => submitHandler(values)} */
-    >
-      { formApi => (
-        <form onSubmit={formApi.submitForm} id="review-form" className="">
-          <label htmlFor="Title">Title</label>
-          <Text placeholder="title" field="title" id="title" />
-          <RadioGroup field="rating">
-            { group => (
-              <div>
-                <label htmlFor="1" className="">1</label>
-                <Radio group={group} value="1" id="1" className="" />
-                <label htmlFor="2" className="">2</label>
-                <Radio group={group} value="2" id="2" className="" />
-                <label htmlFor="3" className="">3</label>
-                <Radio group={group} value="3" id="3" className="" />
-                <label htmlFor="4" className="">4</label>
-                <Radio group={group} value="4" id="4" className="" />
-                <label htmlFor="5" className="">5</label>
-                <Radio group={group} value="5" id="5" className="" />
-              </div>
-            )}
-          </RadioGroup>
-          <label htmlFor="description">Description</label>
-          <Text field="description" id="description" />
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-      )}
-    </Form>,
-  ];
-};
+const ReviewModal = withFormik({
+  // Transform outer props into form values
+  // mapPropsToValues: props => ({ title: '', description: '' }),
+  // Add a custom validation function (this can be async too!)
+  validate: (values, props) => {
+    const errors = {};
+    if (!values.title) {
+      errors.title = 'Required';
+    }
+    return errors;
+  },
+  // Submission handler
+  handleSubmit: (
+    values,
+    {
+      props,
+      setSubmitting,
+      setErrors /* setValues, setStatus, and other goodies */,
+    },
+  ) => {
+    console.log('cool', values);
+  },
+})(InnerForm);
 
 export default ReviewModal;
